13054c001e1bc3b4269a8c79ab257ef6bb828ee6
Tiago Vinícius <tiagovinicyus1996@gmail.com>
2023-03-18 18:45:24 +0000

    update javascript

--- script.js
+++ script.js
@@ -84,6 +84,102 @@ bikes.forEach((bikeMark, indice) => {
             bikeYear.innerHTML = '<strong>2016</strong>'
             bikeFuel.innerHTML = '<strong>Gasoline</strong>'
         }
+
+        localStorage.setItem ('Bike Picture', indice)
     })
 
 })
+
+function openBooking() {
+    let booking = document.getElementById('booking')
+    let bikeName1 = document.getElementById('bikeName')
+    let bikeMarkName1 = document.getElementById('bikeMarkName')
+    let username1 = document.getElementById('username')
+    let name = String(bikeName1.innerText)
+    let mark = String(bikeMarkName1.innerText)
+    let username = String(username1.value)
+
+    if (username.length > 0) {
+        localStorage.setItem('Bike Name', name)
+        localStorage.setItem('Bike Mark', mark)
+        localStorage.setItem('Username', username)
+
+        alert(`Let's Ride, ${username}!`)
+
+        booking.style.display = 'block'
+        location.href = '#booking'
+    } else {
+        alert('Write your name, please!')
+    }
+}
+
+function sendRequest() {
+    let pickuplocation = document.getElementById('pickup')
+    let dropoflocation = document.getElementById('dropof')
+    let pickupDate1 = document.getElementById('pickupDate')
+    let dropofDate1 = document.getElementById('dropofDate')
+
+    let pickupvalue = pickuplocation.options[pickuplocation.selectedIndex].value
+    let dropofvalue = dropoflocation.options[dropoflocation.selectedIndex].value
+    let pickupDate = String(pickupDate1.value)
+    let dropofDate = String(dropofDate1.value)
+    let pickDate = pickupDate.split('-').reverse().join('/')
+    let dropDate = dropofDate.split('-').reverse().join('/')
+
+    if (pickupDate.length == 0 || dropofDate.length == 0) {
+        alert('Insert date, please!')
+    } else {
+        localStorage.setItem('Pick-up Location', pickupvalue)
+        localStorage.setItem('Drop-of Location', dropofvalue)
+        localStorage.setItem('Pick-up Date', pickDate)
+        localStorage.setItem('Drop-of Date', dropDate)
+        location.href = 'finishbook.html'
+    }
+}
+
+function cancelBooking() {
+    let booking = document.getElementById('booking')
+
+    booking.style.display = 'none'
+}
+
+function loadData() {
+    let username = document.getElementById('usernameBook')
+    let bikeModel = document.getElementById('bikemodelBook')
+    let bikeMarkName = document.getElementById('bikemarkBook')
+    let pickupDate = document.getElementById('pickupDateBook')
+    let dropofDate = document.getElementById('dropofDateBook')
+    let pickuplocation = document.getElementById('pickupLocationBook')
+    let dropoflocation = document.getElementById('dropofLocationBook')
+    let bikePicture = document.getElementById('bikeChosen')
+    
+    username.innerText = localStorage.getItem('Username')
+    bikeModel.innerText = localStorage.getItem('Bike Name')
+    bikeMarkName.innerText = localStorage.getItem('Bike Mark')
+    pickupDate.innerText = localStorage.getItem('Pick-up Date')
+    dropofDate.innerText = localStorage.getItem('Drop-of Date')
+    pickuplocation.innerText = localStorage.getItem('Pick-up Location')
+    dropoflocation.innerText = localStorage.getItem('Drop-of Location')
+    let indice = localStorage.getItem('Bike Picture')
+
+    
+
+
+    if (indice == 0) {
+        bikePicture.style.backgroundImage = 'url(imagens/iron883.png)'
+    } else if (indice == 1) {
+        bikePicture.style.backgroundImage = 'url(imagens/fortyeight.png)'
+    } else if (indice == 2) {
+        bikePicture.style.backgroundImage = 'url(imagens/ftr1200.png)'
+    } else if (indice == 3 ) {
+        bikePicture.style.backgroundImage = 'url(imagens/bmwk1300s.png)'
+    } else if (indice == 4) {
+        bikePicture.style.backgroundImage = 'url(imagens/ducatienduro.png)'
+    } else if (indice == 5 ){
+        bikePicture.style.backgroundImage = 'url(imagens/ducatimonster1200.png)'
+    }
+}
+
+function finishBooking() {
+    location.href = 'index.html'
+}
